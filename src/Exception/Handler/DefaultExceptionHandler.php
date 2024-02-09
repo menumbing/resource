06c1526d2e1096461102bb9a33f5ce0047e8410e
Pandawa <developer@pandawa.io>
2024-02-09 07:16:43 +0000

    feat(resource): add default exception handler

---
+++ src/Exception/Handler/DefaultExceptionHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Menumbing\Resource\Exception\Handler;
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\ExceptionHandler\ExceptionHandler;
+use Hyperf\HttpMessage\Exception\HttpException;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Menumbing\Resource\Contract\ResourceStrategyInterface;
+use Swow\Psr7\Message\ResponsePlusInterface;
+use Throwable;
+
+/**
+ * @author  Iqbal Maulana <iq.bluejack@gmail.com>
+ */
+class DefaultExceptionHandler extends ExceptionHandler
+{
+    #[Inject]
+    protected ResourceStrategyInterface $resource;
+
+    public function handle(Throwable $throwable, ResponsePlusInterface $response)
+    {
+        $this->stopPropagation();
+
+        if ($this->resource->supports($throwable)) {
+            $response = $this->resource->render($throwable)->toResponse();
+            $response->setStatus($this->getStatusCode($throwable));
+
+            return $response;
+        }
+
+        return $response
+            ->setStatus($this->getStatusCode($throwable))
+            ->setBody(new SwooleStream($throwable->getMessage()));
+    }
+
+    public function isValid(Throwable $throwable): bool
+    {
+        return true;
+    }
+
+    protected function getStatusCode(Throwable $throwable): int
+    {
+        if ($throwable instanceof HttpException) {
+            return $throwable->getStatusCode();
+        }
+
+        if ($throwable->getCode()
+            && is_int($throwable->getCode())
+            && $throwable->getCode() >= 400
+            && $throwable->getCode() < 500) {
+            return $throwable->getCode();
+        }
+
+        return 500;
+    }
+}
